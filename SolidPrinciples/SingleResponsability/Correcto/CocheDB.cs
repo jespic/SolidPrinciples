ff8747cd811bebdc6a4d3cf6cf8d746b92fa9a1d
CORP\jespical <javiespicalabuig@gmail.com>
2021-12-21 17:52:51 +0100

    Single responsability principle implemented

---
+++ SolidPrinciples/SingleResponsability/Correcto/CocheDB.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolidPrinciples.SingleResponsability.Correcto
+{
+    class CocheDB
+    {
+        void guardarCocheDB(Coche coche)
+        {
+
+        }
+        void eliminarCocheDB(Coche coche)
+        {
+
+        }
+    }
+}
---
+++ SolidPrinciples/SingleResponsability/Incorrecto/Coche.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolidPrinciples.SingleResponsability.Incorrecto
+{
+    class Coche
+    {
+        String marca;
+
+        Coche(string marca)
+        {
+            this.marca = marca;
+        }
+        string getMarcaCoche()
+        {
+            return marca;
+        }
+
+        void guardarCocheDB(Coche coche)
+        {
+            
+        }
+    }
+}
