1ec9e6e86e8d3cec3bc49f7ec0060246487907f5
CORP\jespical <javiespicalabuig@gmail.com>
2021-12-22 17:28:06 +0100

    Interface Segregation Incorrect Example Reference #7

---
+++ SolidPrinciples/InterFaceSegregation/Incorrecto/IAve.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolidPrinciples.InterFaceSegregation.Incorrecto
+{
+    public interface IAve
+    {
+        void volar();
+        void comer();
+        void nadar();
+    }
+}
