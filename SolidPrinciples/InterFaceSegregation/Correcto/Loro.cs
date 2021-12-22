5ab78b216200a8883a25ab1c24a29e079c875ab1
CORP\jespical <javiespicalabuig@gmail.com>
2021-12-22 17:29:07 +0100

    Interface Segregation Correct Example Closes #7

---
+++ SolidPrinciples/InterFaceSegregation/Correcto/Loro.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolidPrinciples.InterFaceSegregation.Correcto
+{
+    public class Loro : IAve, IAveVoladora
+    {
+        public void volar()
+        {
+            //...
+        }
+
+        public void comer()
+        {
+            //..
+        }
+    }
+}
